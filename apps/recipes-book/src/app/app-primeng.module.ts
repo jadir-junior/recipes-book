24f921681ee750b652784bf36896f21da09f7b4a
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-05-09 16:14:38 -0300

    add primeng framework

--- apps/recipes-book/src/app/app-primeng.module.ts
+++ apps/recipes-book/src/app/app-primeng.module.ts
@@ -1,7 +1,8 @@
+import { ButtonModule } from 'primeng/button';
 import { DataViewModule } from 'primeng/dataview';
 import { NgModule } from '@angular/core';
 
-const modules = [DataViewModule];
+const modules = [DataViewModule, ButtonModule];
 
 @NgModule({
   exports: [...modules],
--- apps/recipes-book/src/app/app.module.ts
+++ apps/recipes-book/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { AppComponent } from './app.component';
 import { AppPrimengModule } from './app-primeng.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RecipeModule } from './recipe/recipe.module';
@@ -13,6 +14,7 @@ import { RecipeModule } from './recipe/recipe.module';
     AppRoutingModule,
     AppPrimengModule,
     HttpClientModule,
+    CoreModule,
     RecipeModule,
   ],
   providers: [],
