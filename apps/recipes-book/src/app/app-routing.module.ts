8018a474e400dcfaf83901f1b37a9e12f4c29b59
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-05-10 15:02:41 -0300

    error handling in action

--- apps/recipes-book/src/app/app-routing.module.ts
+++ apps/recipes-book/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
@@ -12,7 +14,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), BrowserAnimationsModule],
+  providers: [MessageService],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
